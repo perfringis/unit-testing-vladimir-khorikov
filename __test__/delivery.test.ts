a7288879cbe4d5c31ff22d90e9f0530195392ffe
perfringis <kopko.mateusz@gmail.com>
2025-02-11 10:48:35 +0100

    add exmaple

---
+++ __test__/delivery.test.ts
@@ -0,0 +1,12 @@
+describe('DeliveryTests', () => {
+  test.each([[-1], [0], [1]])(
+    'detects an invalid delivery date',
+    (daysFromNow) => {
+      // your test here
+    },
+  );
+
+  test('the soonest delivery date is two days from now', () => {
+    // your test here
+  });
+});
